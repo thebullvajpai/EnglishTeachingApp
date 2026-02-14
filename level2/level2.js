998a2a5ef8782325351a6c5e43357a84a3cea513
VAJPAI <167130584+thebullvajpai@users.noreply.github.com>
2026-02-14 21:15:47 +0530

    Add files via upload

---
+++ level2/level2.js
@@ -0,0 +1,32 @@
+const container = document.getElementById("practiceContainer");
+
+// Check if Level 1 completed
+// const level1Completed = localStorage.getItem("level1_completed");
+// const level1Completed = true;
+
+if (!level1Completed) {
+  container.innerHTML = `
+    <div class="card locked">
+      <h3>🔒 Level 2 Locked</h3>
+      <p>Complete Level 1 first.</p>
+    </div>
+  `;
+} else {
+  container.innerHTML = `
+    <div class="card">
+      <h3>🔀 Rearrange Sentences</h3>
+      <p>Practice arranging words in correct order.</p>
+      <button onclick="location.href='sentence_rearrange.html'">
+        Start Practice
+      </button>
+    </div>
+
+    <div class="card">
+      <h3>🌍 Translate Sentences</h3>
+      <p>Translate Hindi sentences into English.</p>
+      <button onclick="location.href='sentence_translate.html'">
+        Start Practice
+      </button>
+    </div>
+  `;
+}
